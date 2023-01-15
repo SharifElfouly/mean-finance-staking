b842a37b8ab26c89c5a927ca11ee97cc29ea3d42
shafu.eth <selfouly@gmail.com>
2023-01-15 14:26:15 +0100

    permission manager

--- src/Staking.sol
+++ src/Staking.sol
@@ -2,14 +2,41 @@
 pragma solidity = 0.8.17;
 
 import {IDCAHub, IDCAHubPositionHandler} from "../src/interfaces/IDCAHub.sol";
+import {IDCAPermissionManager} from "../src/interfaces/IDCAPermissionManager.sol";
+import {IERC20} from "../src/interfaces/IERC20.sol";
 import "forge-std/console.sol";
 
 contract Staking {
+  IDCAHub               public immutable hub;
+  IDCAPermissionManager public immutable permissionManager;
+  IERC20                public immutable rewardToken;
 
-  constructor(IDCAHub _hub) {
-    IDCAHubPositionHandler.UserPosition memory p = _hub.userPosition(7);
-    console.logAddress(address(p.to));
-    console.logAddress(address(p.from));
+  constructor(
+      IDCAHub               _hub,
+      IDCAPermissionManager _permissionManager,
+      IERC20               _rewardToken
+  ) {
+      hub               = _hub;
+      permissionManager = _permissionManager;
+      rewardToken       = _rewardToken;
   }
 
+  function stake(uint tokenId) external {
+    permissionManager.transferFrom(msg.sender, address(this), tokenId);
+
+  }
+
+  function claim() external {
+
+  }
+
+  function unstake() external {
+
+  }
+
+  // function getPos() public {
+  //     // IDCAHubPositionHandler.UserPosition memory p = _hub.userPosition(7);
+  //     // console.logAddress(address(p.to));
+  //     // console.logAddress(address(p.from));
+  // }
 }
--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -4,12 +4,23 @@ pragma solidity = 0.8.17;
 import "forge-std/Test.sol";
 import {Staking} from "../src/Staking.sol";
 import {IDCAHub} from "../src/interfaces/IDCAHub.sol";
+import {IDCAPermissionManager} from "../src/interfaces/IDCAPermissionManager.sol";
+import {IERC20} from "../src/interfaces/IERC20.sol";
+
+address constant HUB                = 0xA5AdC5484f9997fBF7D405b9AA62A7d88883C345;
+address constant PERMISSION_MANAGER = 0x20bdAE1413659f47416f769a4B27044946bc9923;
+address constant REWARD_TOKEN       = 0xA5AdC5484f9997fBF7D405b9AA62A7d88883C345;
 
 contract StakingTest is Test {
-  address HUB = 0xA5AdC5484f9997fBF7D405b9AA62A7d88883C345;
+
+  Staking public staking;
 
   function setUp() public {
-    new Staking(IDCAHub(HUB));
+    staking = new Staking(
+      IDCAHub(HUB),
+      IDCAPermissionManager(PERMISSION_MANAGER),
+      IERC20(REWARD_TOKEN)
+    );
   }
 
   function testStake() public {
